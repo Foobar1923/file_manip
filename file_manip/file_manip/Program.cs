e04e78e8036052d29383355376e4fb5054145c76
Foobar1923 <kyates1923@gmail.com>
2018-11-21 09:54:04 -0600

    Test upload

--- file_manip/file_manip/Program.cs
+++ file_manip/file_manip/Program.cs
@@ -10,12 +10,23 @@ namespace file_manip
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            /*
+            
+            In the console:
+            1) Prompt a user for a file path to a text file (actually a .cs file, but we'll get to that in a moment)
+            2) read the text file and remove all commented lines from it
+            3) save the text file in the same location with a new name of "OLDNAME"-min.cs
+            4) open the new file in Notepad
+            5) end prog 
+            
+            */
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Enter path of file to open: ");
+            string pathName = Console.ReadLine(pathName);
+            Console.WriteLine(pathName);
+
+
+
         }
     }
 }
