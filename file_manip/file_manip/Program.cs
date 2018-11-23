f1efbfd0139b5f003deb772aff7e6b8f53f53369
Foobar1923 <16374806+Foobar1923@users.noreply.github.com>
2018-11-23 00:31:02 -0600

    Reads file and counts commented lines

--- file_manip/file_manip/Program.cs
+++ file_manip/file_manip/Program.cs
@@ -11,27 +11,41 @@ namespace file_manip
     {
         static void Main(string[] args)
         {
-            /*
-            
+            /*            
+             *            
             In the console:
             1) Prompt a user for a file path to a text file (actually a .cs file, but we'll get to that in a moment)
             2) read the text file and remove all commented lines from it
             3) save the text file in the same location with a new name of "OLDNAME"-min.cs
             4) open the new file in Notepad
-            5) end prog 
+            5) end program    
             
             */
 
             string pathName;
-            string choiceAnswer;
+            int count = 1;
+            string searchText = "//";
+            string[] lines = System.IO.File.ReadAllLines(pathName);
 
             Console.WriteLine("Enter path of file to open: ");
             pathName = Console.ReadLine();
-            Console.WriteLine("Is this path correct?: {0}", pathName);
-            choiceAnswer = Console.ReadLine();
-            if (choiceAnswer == "yes" || choiceAnswer == "y")
-                Process.Start("notepad.exe",pathName);
             
+            foreach (string line in lines)
+            {
+                string lineSearch = line;
+                bool result = lineSearch.Contains(searchText);
+                if(result)
+                {
+                    //int index = subLine.IndexOf(searchText);
+                    //if(index >= 0)
+                    Console.WriteLine(count);
+                    count++; // counts number of commented lines successfully
+                }                
+            }
+
+            Console.WriteLine(count);
+            //opens notepad with given file name
+            //Process.Start("notepad.exe", pathName);
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
